cd6271ab0f2738f109be193462ebc37257b4f2e0
Dawid-Wilk <wilkdawid.1995@gmail.com>
2021-09-27 14:25:21 +0200

    Manual Slider
    Manual Slider

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ links.forEach(link => {
         link.classList.add('active');
     })
 })
+
+// slider
+
+const slides = document.querySelectorAll('.slide');
+const btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+//manual slider 
+const manualSlider = function(manual){
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+    btns.forEach((btn) => {
+        btn.classList.remove('active');
+        })
+    })
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+    btn.addEventListener('click', () => {
+        manualSlider(i);
+        currentSlide = i;
+    })
+})
